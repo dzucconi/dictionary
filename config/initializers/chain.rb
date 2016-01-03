2c2cf4ede42df678a61e14f64286026f98efec58
dzucconi <mail@damonzucconi.com>
2016-01-03 00:06:37 -0500

    Removes sample usage quotes; clean up

--- config/initializers/chain.rb
+++ config/initializers/chain.rb
@@ -1,14 +1,15 @@
 class Chain
   CHAINS = {
     red: 'data.verb',
-    blue: 'data.noun'
+    blue: 'data.noun',
+    black: 'data.adj'
   }
 
   SOURCE = File.expand_path("../#{CHAINS[ENV['COLOR'].to_sym]}", __FILE__)
 
   def self.take(n = 1)
     n = n.nil? || n.zero? ? 1 : n
-    fragments = File.readlines(SOURCE).sample(n).first.downcase.split(/,|;/)
-      [[fragments].flatten.sample.gsub('"', '')]
+    fragments = File.readlines(SOURCE).sample(n).first.downcase.split(';')
+      [[fragments].flatten.sample]
   end
 end
